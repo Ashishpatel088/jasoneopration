7997f0e71062cc5f5c6ef1705a9cff5c47cf8832
patelashish088 <patelashish088@gmail.com>
2022-04-19 14:04:57 +0530

    jasonOpration

---
+++ index.js
@@ -0,0 +1,290 @@
+    const data = {
+        
+		"columns": {
+			"id": "Id",
+			"customer_id": "DEB-Auto",
+			"name": "Name",
+			"labelName": "Label",
+			"address": "Address",
+			"email": "Email",
+			"last_update": "Last Update"
+		},
+		"searchableColumns": ["customer_id", "name", "address"],
+		"sortableColumns": ["id", "customer_id", "name", "address", "email", "last_update"],
+		"organizations": [{
+			"id": 731,
+			"customer_id": "1427",
+			"name": "Test ajay",
+			"address": "PosBill GmbH, Brunnengasse, Kehlbach, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:11:08.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 867,
+			"bankaccount_info2": null,
+			"test":'abcdhd'
+		}, {
+			"id": 763,
+			"customer_id": "1417",
+			"name": "test-QA1",
+			"address": "German Experts Car Maintenance - Abu Dhabi - United Arab Emirates",
+			"email": null,
+			"last_update": "2022-03-30T07:02:32.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 883,
+			"bankaccount_info2": null
+		}, {
+			"id": 755,
+			"customer_id": "1418",
+			"name": "Atest4",
+			"address": "Gera, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:02:06.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 879,
+			"bankaccount_info2": null
+		}, {
+			"id": 751,
+			"customer_id": "1419",
+			"name": "Atest2",
+			"address": null,
+			"email": null,
+			"last_update": "2022-03-30T07:01:46.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 877,
+			"bankaccount_info2": null
+		}, {
+			"id": 730,
+			"customer_id": "1426",
+			"name": "test sevDesk m1",
+			"address": "PosBill GmbH, Brunnengasse, Kehlbach, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:00:42.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 866,
+			"bankaccount_info2": null
+		}, {
+			"id": 747,
+			"customer_id": "1432",
+			"name": "test-shivani3",
+			"address": "Germany",
+			"email": "shivani@test.com",
+			"last_update": "2022-03-30T06:59:58.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 875,
+			"bankaccount_info2": null
+		}, {
+			"id": 862,
+			"customer_id": "1485",
+			"name": "Gellert Imbiss",
+			"address": "Quellenstrasse 77 , Wien, 1100, AT Avusturya",
+			"email": "sales@gellertimbiss.com",
+			"last_update": "2022-03-02T09:14:07.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 947,
+			"bankaccount_info2": null
+		}, {
+			"id": 863,
+			"customer_id": "1486",
+			"name": "test1.0",
+			"address": "",
+			"email": "ngvdungit2@maildrop.cc",
+			"last_update": "2022-03-09T09:24:57.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 948,
+			"bankaccount_info2": null
+		}, {
+			"id": 859,
+			"customer_id": "1482",
+			"name": "R5",
+			"address": "Sinabelkirchen 8, Sinabelkirchen, 8261,",
+			"email": "weberhofer.andreas@gmail.com",
+			"last_update": "2022-03-02T07:08:32.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 943,
+			"bankaccount_info2": null
+		}, {
+			"id": 860,
+			"customer_id": "1483",
+			"name": "Onassi",
+			"address": "Langemarckstr. 171, Bremen, 28199, DE Deutschland",
+			"email": "lpoetsch@rea.de",
+			"last_update": "2022-03-02T07:30:34.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 945,
+			"bankaccount_info2": null
+		}, {
+			"id": 861,
+			"customer_id": "1484",
+			"name": "nguyen",
+			"address": "",
+			"email": "ngvdung90it@maildrop.cc",
+			"last_update": "2022-03-02T08:46:46.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 946,
+			"bankaccount_info2": null
+		}, {
+			"id": 856,
+			"customer_id": "1479",
+			"name": "Ruchita 6291",
+			"address": "3B, Innenstadt, 14473, Germany",
+			"email": "ruchitas@scaletech.xyz",
+			"last_update": "2022-03-02T06:51:40.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 940,
+			"bankaccount_info2": null
+		}, {
+			"id": 857,
+			"customer_id": "1480",
+			"name": "Alois Geyer",
+			"address": "Mitterfeldgasse 21, Ziersdorf, 3710,",
+			"email": "aloisgeyer@gmx.at",
+			"last_update": "2022-03-02T07:05:46.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 941,
+			"bankaccount_info2": null
+		}, {
+			"id": 858,
+			"customer_id": "1481",
+			"name": "Alois Geyer",
+			"address": "Mitterfeldgasse 21, Ziersdorf, 3710,",
+			"email": "aloisgeyer@gmx.at",
+			"last_update": "2022-03-02T07:07:31.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 942,
+			"bankaccount_info2": null
+		}, {
+			"id": 849,
+			"customer_id": "1475",
+			"name": "R1",
+			"address": "",
+			"email": "",
+			"last_update": "2022-03-01T12:29:01.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 936,
+			"bankaccount_info2": null
+		}]
+    }
+
+//  get headder data
+let thid = document.getElementById('tableData')
+let trid = document.getElementById('trdata')
+let userData = data['organizations'];
+// console.log(userData,'user');
+let searObj = data['searchableColumns'];
+ console.log(searObj,'searchobj');
+let newData = '';
+let newvar =data['columns'];
+// console.log(newvar,'dddddd');
+ for(let key of Object.values(newvar)) {
+    
+	//console.log(key,'key log');
+	newData += `<th>${key}<button type="submit" id="sortData" value="sort">sort</button></th>`;
+	
+
+ }
+ trid.innerHTML = `<tr id="searchrow"></tr>`
+ let newval = document.getElementById('searchrow');
+
+ console.log(newval,'hshsh');
+ console.log(searObj);
+ Object.keys(newvar).forEach(col => {
+	 console.log(col);
+	const find =searObj.findIndex(element => element === col);
+	console.log(find>=0);
+	 if(find >= 0) {
+		newval.innerHTML += `<td><input type="text" /></td>`
+	 } else {
+		 newval.innerHTML += `<td></td>`
+	 }
+
+ })
+ /* Object.keys(newvar).forEach(findcolu => {
+	 console.log(findcolu,'searchval');
+	 let searboxval = '';
+	 searObj.forEach(searchval => {
+		 console.log(findcolu,searchval === findcolu, 'findcolu');
+		 let searchtdId = document.getElementById('newSearchBox');
+		 if(searchval === findcolu) {
+			 //console.log(searboxval.innerHTML ='abc' ,"searchboxval");
+			 let newsearchboxval = '';
+			 newsearchboxval = `<input type="text" />`
+			//  searchtdId.innerHTML += 
+			 searboxval +=`<td id="newSearchBox">${newsearchboxval}</td>`
+			// trid.innerHTML = `<td></td>`
+
+		 } else {
+			searboxval += `<td></td>`
+		 }
+	 })
+	 newval.innerHTML = `<tr>${searboxval}</tr>`
+ }) */
+/* for(let i=0;i<searObj.length;i++) {
+	console.log(i,'valu');
+} */
+ /* if(newvar.customer_id = 'DEB-Auto') {
+		
+	newData += `<tr><td></td><td><input type="text" onkeyup="searchBox(1,'cust_SearchBox')" id="cust_SearchBox" placeholder="search cust_id"><td><input type="text" onkeyup="searchBox(2,'searchBox')" id="searchBox" placeholder="search Name"></td><td></td><td><input type="text" id="searchBox1" onkeyup="searchBox(4,'searchBox1')" placeholder="search Adress"></td><td></td><td></td></td></tr>`
+} */
+ thid.innerHTML += newData;
+// console.log(newData,'olddata')
+let newuserdata = '';
+userData.forEach(ele => {
+	let addUserData = '';
+	// console.log(ele,'elelog');
+	Object.keys(newvar).forEach(colunmkey => {
+		// console.log(colunmkey,'colunmkey');
+		 addUserData += `<td>${ele[colunmkey]}</td>`
+		// console.log(addUserData,'newcolumnupdtae');
+	});
+	trid.innerHTML += `<tr>${addUserData}</tr>`
+});
+/* for(let datainfo of Object.values(userData)) {
+
+    console.log(datainfo,'userddddddta');
+    
+    newuserdata += `<tr><td>${datainfo.id}</td><td>${datainfo.customer_id}</td><td>${datainfo.name}</td><td>${datainfo.labelName}</td><td>${datainfo.address}</td><td>${datainfo.email}</td><td>${datainfo.last_update}</td></tr>`
+}
+console.log(newuserdata,'kkk');
+trid.innerHTML += newuserdata; */
+/* let searchbtn = '';
+for(let search of userData) {
+    if(search.name && search.address) {
+        searchbtn += `<td></td><td></td><td><input type="text" /></td>` 
+    }
+} */
+function searchBox(columnNumber,id) {
+	let searchData = document.getElementById(id); 
+	var input, filter, table, tr, td, i, txtValue;
+	//input = document.getElementById("myInput");
+	filter = searchData.value.toUpperCase();
+	table = document.getElementById("trdata");
+	tr = table.getElementsByTagName("tr");
+	for (i = 0; i < tr.length; i++) {
+	  td = tr[i].getElementsByTagName("td")[columnNumber];
+	
+	  if (td) {
+		txtValue = td.textContent || td.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		  tr[i].style.display = "";
+		} else {
+		  tr[i].style.display = "none";
+		}
+	  }       
+	}
+  
+}
